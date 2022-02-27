008f336f12024723989813c6099789faec839a8d
LINYUHAN1288 <1057000273@qq.com>
2022-02-28 00:24:11 +0800

    feat: 新增 cleanObject

---
+++ src/screens/list/search-pannel.tsx
@@ -0,0 +1,9 @@
+import { Form, Input } from "antd";
+
+export const SearchPannel = () => {
+    return (
+        <Form style={{ marginBottom: "2rem" }} layout={"inline"}>
+            <Form.Item></Form.Item>
+        </Form>
+    );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,6 +11,20 @@ export const useMount = (callback: () => void) => {
     }, [callback]);
 };
 
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+    if (!object) {
+        return {};
+    }
+    const result = { ...object };
+    Object.keys(result).forEach((key) => {
+        const value = result[key];
+        if (isVoid(value)) {
+            delete result[key];
+        }
+    });
+    return result;
+};
+
 export const useDebounce = <V>(value: V, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
