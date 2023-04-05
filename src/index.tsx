f20f49f7a96118d5ec528607a3f93eed4208938b
LLinYuHan <1057000273@qq.com>
2023-04-05 22:13:08 +0800

    feature: format style

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { AppProviders } from "context";
 import reportWebVitals from "./reportWebVitals";
-import "antd/dist/antd.less";
+import { loadDevTools } from "jira-dev-tool";
+// import "antd/dist/antd.less";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <AppProviders>
-            <App />
-        </AppProviders>
-    </React.StrictMode>,
-    document.getElementById("root")
+loadDevTools(() =>
+    ReactDOM.render(
+        <React.StrictMode>
+            <AppProviders>
+                <App />
+            </AppProviders>
+        </React.StrictMode>,
+        document.getElementById("root")
+    )
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/list/list-panel.tsx
+++ src/pages/list/list-panel.tsx
@@ -5,25 +5,80 @@
 
 // import React from "react";
 import { User } from "types/user";
-import { TableProps, Table } from "antd";
+import { TableProps, Table, Modal, Dropdown, Menu } from "antd";
 import { Project } from "types/project";
 import { Pin } from "components/pin";
+import { Link } from "react-router-dom";
+import { ButtonNoPadding } from "components/lib";
 
 interface ListProps extends TableProps<Project> {
     users: User[];
 }
 
-const columns = [
-    {
-        title: <Pin checked={true} disabled={true} />,
-        render: (value: unknown, project: Project) => {
-            return <Pin checked={project.pin} />;
-        }
-    }
-];
-
 export const ListPanel = ({ users, ...props }: ListProps) => {
     return (
-        <Table rowKey={"id"} pagination={false} columns={columns} {...props} />
+        <Table
+            rowKey={"id"}
+            pagination={false}
+            columns={[
+                {
+                    title: <Pin checked={true} disabled={true} />,
+                    render: (value: unknown, project: Project) => {
+                        return <Pin checked={project.pin} />;
+                    }
+                },
+                {
+                    title: "名称",
+                    render: (value: unknown, project: Project) => {
+                        return <Link to={`projects/${String(project.id)}`}>{project.name}</Link>;
+                    }
+                },
+                {
+                    title: "部门",
+                    dataIndex: "organization"
+                },
+                {
+                    title: "负责人",
+                    render: (value: unknown, project: Project) => {
+                        return <span>{users.find((user) => user.id === project.personId)?.name || "未知"}</span>;
+                    }
+                },
+                {
+                    title: "创建时间",
+                    render: (value, project) => {
+                        return <span>{project.created ? dayjs(project.created).format("YYYY-MM-DD") : "无"}</span>;
+                    }
+                },
+                {
+                    render: (value, project) => {
+                        return <More project={project} />;
+                    }
+                }
+            ]}
+            {...props}
+        />
+    );
+};
+
+const More = ({ project }: { project: Project }) => {
+    const deleteProject = (id: number) => {
+        Modal.confirm({
+            title: "delete",
+            okText: "yes"
+        });
+    };
+    return (
+        <Dropdown
+            overlay={
+                <Menu>
+                    <Menu.Item>编辑</Menu.Item>
+                    <Menu.Item onClick={() => deleteProject(project.id)} key={"delete"}>
+                        删除
+                    </Menu.Item>
+                </Menu>
+            }
+        >
+            <ButtonNoPadding type={"link"}>...</ButtonNoPadding>
+        </Dropdown>
     );
 };
